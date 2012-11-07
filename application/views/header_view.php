f9c0f036c14b16a44ee6c291631645bc44bbcc33
paparts <icpep.org@gmail.com>
2012-11-07 23:48:23 +0800

    Added base_url

---
+++ application/views/header_view.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Bootstrap, from Twitter</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- Le styles -->
+    <link href="<?=base_url()?>css/bootstrap.css" rel="stylesheet">
+    <link href="<?=base_url()?>css/bootstrap-responsive.css" rel="stylesheet">
+    <link href="<?=base_url()?>css/global.css" rel="stylesheet">
+    <link href="<?=base_url()?>css/font-awesome.css" rel="stylesheet">
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+    <script>var base_url = "<?=base_url().index_page()?>";</script>
+  </head>
+  <body>
